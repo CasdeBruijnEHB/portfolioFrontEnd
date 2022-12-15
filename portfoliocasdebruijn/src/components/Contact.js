985bc1b8985e49328fafd482f2c3a0ebf74a26d2
Cas de Bruijn <71756854+CasdeBruijnEHB@users.noreply.github.com>
2022-12-15 11:53:49 +0100

    filter werkend

--- portfoliocasdebruijn/src/components/Contact.js
+++ portfoliocasdebruijn/src/components/Contact.js
@@ -4,6 +4,7 @@ class Contact extends Component {
     render() {
         return (
             <div className="fullPage">
+                <p className="subtitle">Contact me</p>
                 <p>Want to work with me?</p>
                 <p>Get in touch</p>
             </div>
--- portfoliocasdebruijn/src/components/DataFetching.js
+++ portfoliocasdebruijn/src/components/DataFetching.js
@@ -2,24 +2,67 @@ import React, {useState, useEffect} from 'react';
 import axios from 'axios'
 import '../styles/workpage.css'
 
+
 function DataFetching(props) {
     const [posts, setPosts] = useState([])
+    const [filteredlist, setFilteredList]=useState([])
+    const [filterState,setFilterState]=useState("all");
+
     useEffect(()=>{
         
        axios.get('http://localhost:1337/api/Projects?populate=*')
        .then(res=>{
-        res.data.data.map(post=>(console.log(post.attributes.image.data.attributes.url)))
+        //res.data.data.map(post=>(console.log(post.attributes.image.data.attributes.url)))
         setPosts(res.data.data)
+        setFilteredList(res.data.data)
        })
        .catch(err=>{
         console.log("error! " + err)
        })
     },[])
+
+    function showData(type){
+        if(type==="all"){
+            console.log("all!")
+            setFilteredList(posts);
+        }else if(type==="graphicdesign"){
+            console.log("graphicdesign!")
+
+        const filtered = posts.filter(post => {
+            return post.attributes.category === 'Graphicdesign';
+         });
+         setFilteredList(filtered);
+
+        }else if(type==="webdesign"){
+            console.log("webdesign!")
+
+            const filtered = posts.filter(post => {
+                return post.attributes.category === 'Webdesign';
+             });
+             setFilteredList(filtered);
+        }else if(type==="videodesign"){
+            console.log("videodesign!")
+            const filtered = posts.filter(post => {
+                return post.attributes.category === 'Videodesign';
+             });
+             setFilteredList(filtered);
+        }else{
+            console.log("all!")
+            setFilteredList(posts);
+        }
+    }
+
     return (
         <div className="fullPage">
-            <p>Fetch!</p>
+           <p id="workTitle" className="subtitle">My work</p>
+           <div id="navigatieWork">
+            <p onClick={() => showData('all')} className="navItem">All</p>
+            <p onClick={() => showData('graphicdesign')} className="navItem">Graphicdesign</p>
+            <p onClick={() => showData('webdesign')}  className="navItem">Webdesign</p>
+            <p onClick={() => showData('videodesign')}  className="navItem">Videodesign</p>
+          </div>
             <div id="workContainer">
-                {posts.map((post)=>(
+                {filteredlist.map((post)=>(
                     <div className="worksPost" id={`id${post.id}`} key={post.id}>
                         <p  className='worksTitles'>{post.attributes.Title}</p>
                         <p  className='worksDescriptions'>{post.attributes.description}</p>
