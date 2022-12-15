18a0745cb4c32370f778d7f2c757042d9ac878d7
Cas de Bruijn <71756854+CasdeBruijnEHB@users.noreply.github.com>
2022-12-15 11:53:06 +0100

    filter werkend

--- portfoliocasdebruijn/src/components/Workpage.js
+++ portfoliocasdebruijn/src/components/Workpage.js
@@ -1,6 +1,14 @@
-import React, { Component } from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios'
+import '../styles/workpage.css'
+
 
 class Workpage extends Component {
+
+    
+
+
+
     render() {
         return (
             <div className="fullPage">
